b57d486d67f513f1970adecbafc11afbbd7459f8
Emeka Orji <emekapraiseo@gmail.com>
2022-01-10 08:51:13 +0100

    Finished the page-navigation, found a new method 'scrollIntoView()'

---
+++ js/layout.js
@@ -0,0 +1,27 @@
+const nav = document.querySelectorAll(".page-nav a");
+const pagePart = document.querySelectorAll(".main-content [id]");
+nav.forEach((e, i) => e.addEventListener("click", () => {
+  nav.forEach(e => e.classList.remove("active"));
+  e.classList.add("active");
+  pagePart[i].scrollIntoView();
+}));
+["load", "scroll"].forEach(event => {
+  document.addEventListener(event, () => {
+    pagePart.forEach((e, i) => {
+      if (isVisible(e)) {
+        nav.forEach(e => e.classList.remove("active"));
+        nav[i].classList.add("active");
+      }
+    })
+  });
+});
+
+function isVisible (ele) {
+  const { top, bottom } = ele.getBoundingClientRect();
+  const vHeight = (window.innerHeight || document.documentElement.clientHeight);
+
+  return (
+    (top > 0 || bottom > 0) &&
+    top < vHeight / 2
+  );
+}
